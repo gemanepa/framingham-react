95723e7432006824720bd9c924a21d6719c15cfb
gemanepa <contacto@gemanepa.com>
2019-12-07 15:04:10 -0300

    Fixed Head component not receiving props properly

--- src/view-components/Head/index.jsx
+++ src/view-components/Head/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
 
-export default function HeadComponent(translations) {
+export default function HeadComponent(props) {
+  const { translations } = props;
+  const {
+    appname, description, keywords, title
+  } = translations;
   return (
     <Head>
-      <title>{translations.title}</title>
-      <meta name="application-name" content={translations.appname} />
-      <meta name="description" content={translations.description} />
-      <meta name="keywords" content={translations.keywords} />
+      <title>{title}</title>
+      <meta name="application-name" content={appname} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
     </Head>
   );
 }
+
+HeadComponent.propTypes = {
+  translations: PropTypes.exact({
+    appname: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    keywords: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired
+};

48a02872002f4013fcfeffdd4b65df7a8b205f63
gemanepa <contacto@gemanepa.com>
2019-12-08 23:15:35 -0300

    Added form validations e2e cypress tests

--- tests/e2e/cypress/integration/navigation/navigation-containers.spec.js
+++ tests/e2e/cypress/integration/navigation/navigation-containers.spec.js
@@ -6,7 +6,7 @@ context('Navigation | Switching Container Types', () => {
     cy.visit('/');
   });
 
-  it('should render info container and then calcinput container', () => {
+  it('should render Info from NavButton, then CalcInput from NavButton', () => {
     cy.fixture('navselectors').then((object) => {
       //CalcInput Container and Info NavButton should be visible
       cy.get(object.static.mc_calcinput).should('be.visible');
@@ -29,6 +29,65 @@ context('Navigation | Switching Container Types', () => {
 
       cy.get(object.static.nb_infobtn).should('be.visible');
       cy.get(object.static.nb_infobtn).contains('Info');
+    });
+  });
+
+  it('should render Results after filling Form, then CalcInput from GoBack btn', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.mc_calcinput).should('be.visible');
+      cy.get(object.static.form_gender_female).click({ force: true });
+      cy.get(object.static.form_age_select).click({ force: true });
+      cy.get(object.static.form_age_menuitem).click({ force: true });
+      cy.get(object.static.form_hdl_select).click({ force: true });
+      cy.get(object.static.form_hdl_menuitem).click({ force: true });
+      cy.get(object.static.form_ldl_select).click({ force: true });
+      cy.get(object.static.form_ldl_menuitem).click({ force: true });
+      cy.get(object.static.form_totaldl_select).click({ force: true });
+      cy.get(object.static.form_totaldl_menuitem).click({ force: true });
+      cy.get(object.static.form_ta_select).click({ force: true });
+      cy.get(object.static.form_ta_menuitem).click({ force: true });
+      cy.get(object.static.form_wt_select).click({ force: true });
+      cy.get(object.static.form_wt_menuitem).click({ force: true });
+      cy.get(object.static.form_treatment_checkbox).click({ force: true });
+      cy.get(object.static.form_smoking_checkbox).click({ force: true });
+      cy.get(object.static.form_diabetes_checkbox).click({ force: true });
+
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get(object.static.mc_results).should('be.visible');
+
+      cy.get(object.static.results_goback).click({ force: true });
+      cy.get(object.static.mc_calcinput).should('be.visible');
+    });
+  });
+
+  it('should render Results after filling Form, then Info from NavButton, then CalcInput from GoBack btn', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.mc_calcinput).should('be.visible');
+      cy.get(object.static.form_gender_female).click({ force: true });
+      cy.get(object.static.form_age_select).click({ force: true });
+      cy.get(object.static.form_age_menuitem).click({ force: true });
+      cy.get(object.static.form_hdl_select).click({ force: true });
+      cy.get(object.static.form_hdl_menuitem).click({ force: true });
+      cy.get(object.static.form_ldl_select).click({ force: true });
+      cy.get(object.static.form_ldl_menuitem).click({ force: true });
+      cy.get(object.static.form_totaldl_select).click({ force: true });
+      cy.get(object.static.form_totaldl_menuitem).click({ force: true });
+      cy.get(object.static.form_ta_select).click({ force: true });
+      cy.get(object.static.form_ta_menuitem).click({ force: true });
+      cy.get(object.static.form_wt_select).click({ force: true });
+      cy.get(object.static.form_wt_menuitem).click({ force: true });
+      cy.get(object.static.form_treatment_checkbox).click({ force: true });
+      cy.get(object.static.form_smoking_checkbox).click({ force: true });
+      cy.get(object.static.form_diabetes_checkbox).click({ force: true });
+
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get(object.static.mc_results).should('be.visible');
+
+      cy.get(object.static.nb_infobtn).click({ force: true });
+      cy.get(object.static.mc_info).should('be.visible');
+
+      cy.get(object.static.info_goback).click({ force: true });
+      cy.get(object.static.mc_calcinput).should('be.visible');
     });
   });
 });
---
+++ tests/e2e/cypress/integration/navigation/navigation-form.spec.js
@@ -0,0 +1,142 @@
+/* eslint-disable no-undef */ /* eslint-disable spaced-comment */
+/// <reference types="Cypress" />
+
+context('Navigation | Form', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Should show results after filling form', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.mc_calcinput).should('be.visible');
+      cy.get(object.static.form_gender_female).click({ force: true });
+      cy.get(object.static.form_age_select).click({ force: true });
+      cy.get(object.static.form_age_menuitem).click({ force: true });
+      cy.get(object.static.form_hdl_select).click({ force: true });
+      cy.get(object.static.form_hdl_menuitem).click({ force: true });
+      cy.get(object.static.form_ldl_select).click({ force: true });
+      cy.get(object.static.form_ldl_menuitem).click({ force: true });
+      cy.get(object.static.form_totaldl_select).click({ force: true });
+      cy.get(object.static.form_totaldl_menuitem).click({ force: true });
+      cy.get(object.static.form_ta_select).click({ force: true });
+      cy.get(object.static.form_ta_menuitem).click({ force: true });
+      cy.get(object.static.form_wt_select).click({ force: true });
+      cy.get(object.static.form_wt_menuitem).click({ force: true });
+      cy.get(object.static.form_treatment_checkbox).click({ force: true });
+      cy.get(object.static.form_smoking_checkbox).click({ force: true });
+      cy.get(object.static.form_diabetes_checkbox).click({ force: true });
+
+      cy.get(object.static.form_sendbtn).click({ force: true });
+
+      cy.get(object.static.mc_results).should('be.visible');
+    });
+  });
+
+  it('Should fail all inputs validations', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_sendbtn).click({ force: true });
+
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(18); });
+    });
+  });
+
+  it('Should fail all inputs validations except gender', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_gender_female).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(17); });
+    });
+  });
+
+  it('Should fail all inputs validations except age', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_age_select).click({ force: true });
+      cy.get(object.static.form_age_menuitem).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(16); });
+    });
+  });
+
+  it('Should fail all inputs validations except hdl', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_ldl_select).click({ force: true });
+      cy.get(object.static.form_ldl_menuitem).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(15); });
+    });
+  });
+
+  it('Should fail all inputs validations except ldl', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_age_select).click({ force: true });
+      cy.get(object.static.form_age_menuitem).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(16); });
+    });
+  });
+
+  it('Should fail all inputs validations except totaldl', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_totaldl_select).click({ force: true });
+      cy.get(object.static.form_totaldl_menuitem).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(15); });
+    });
+  });
+
+  it('Should fail all inputs validations except ta', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_ta_select).click({ force: true });
+      cy.get(object.static.form_ta_menuitem).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(15); });
+    });
+  });
+
+  it('Should fail all inputs validations except wt when gender selected', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_gender_female).click({ force: true });
+      cy.get(object.static.form_wt_select).click({ force: true });
+      cy.get(object.static.form_wt_menuitem).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(14); });
+    });
+  });
+
+  it('Should fail all inputs validations when wt selected with no gender', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.form_wt_select).click({ force: true });
+      cy.get(object.static.form_wt_menuitem_nogender).click({ force: true });
+      cy.get(object.static.form_sendbtn).click({ force: true });
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(18); });
+    });
+  });
+
+  it('Should clean all fields', () => {
+    cy.fixture('navselectors').then((object) => {
+      cy.get(object.static.mc_calcinput).should('be.visible');
+      cy.get(object.static.form_gender_female).click({ force: true });
+      cy.get(object.static.form_age_select).click({ force: true });
+      cy.get(object.static.form_age_menuitem).click({ force: true });
+      cy.get(object.static.form_hdl_select).click({ force: true });
+      cy.get(object.static.form_hdl_menuitem).click({ force: true });
+      cy.get(object.static.form_ldl_select).click({ force: true });
+      cy.get(object.static.form_ldl_menuitem).click({ force: true });
+      cy.get(object.static.form_totaldl_select).click({ force: true });
+      cy.get(object.static.form_totaldl_menuitem).click({ force: true });
+      cy.get(object.static.form_ta_select).click({ force: true });
+      cy.get(object.static.form_ta_menuitem).click({ force: true });
+      cy.get(object.static.form_wt_select).click({ force: true });
+      cy.get(object.static.form_wt_menuitem).click({ force: true });
+      cy.get(object.static.form_treatment_checkbox).click({ force: true });
+      cy.get(object.static.form_smoking_checkbox).click({ force: true });
+      cy.get(object.static.form_diabetes_checkbox).click({ force: true });
+
+      cy.get(object.static.form_resetbtn).click({ force: true });
+
+      cy.get(object.static.form_sendbtn).click({ force: true });
+
+      cy.get('.Mui-error').then(($lis) => { expect($lis).to.have.length(18); });
+    });
+  });
+});

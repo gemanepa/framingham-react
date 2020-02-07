a59c4b65c973dc5fd611d7152248660d0ab390e7
gemanepa <contacto@gemanepa.com>
2020-02-07 15:44:17 -0300

    Fixed nextjs routes slashers in urls bug

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,18 @@ export default class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, router: { asPath } } = this.props;
+
+    // Next.js currently does not allow trailing slash in a route.
+    // This is a client side redirect in case trailing slash occurs.
+    if (asPath.length > 1 && asPath.endsWith('/')) {
+      const urlWithoutEndingSlash = asPath.replace(/\/*$/gim, '');
+
+      if (typeof window !== 'undefined') {
+        window.location.replace(urlWithoutEndingSlash);
+      }
+      return null;
+    }
 
     return (
       <>
